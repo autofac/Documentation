9f70f4919f299866908ae962fd5a77ab808881c9
Travis Illig <tillig@paraesthesia.com>
2017-09-25 05:41:11 -0700

    Copyright is a range

--- docs/conf.py
+++ docs/conf.py
@@ -44,7 +44,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'Autofac'
-copyright = u'2014, Autofac Contributors'
+copyright = u'2011 - 2017, Autofac Contributors'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
